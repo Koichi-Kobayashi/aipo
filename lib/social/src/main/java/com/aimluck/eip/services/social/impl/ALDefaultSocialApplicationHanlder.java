af27e3da91e51ce5a2976c2810d30fc76a2fbb16
hurts14 <bayern.uver@gmail.com>
2017-10-24 15:11:42 +0900

    refs#41774

--- lib/social/src/main/java/com/aimluck/eip/services/social/impl/ALDefaultSocialApplicationHanlder.java
+++ lib/social/src/main/java/com/aimluck/eip/services/social/impl/ALDefaultSocialApplicationHanlder.java
@@ -1040,35 +1040,37 @@ public class ALDefaultSocialApplicationHanlder extends
             .param("appId", request.getAppId())
             .param("externalId", request.getExternalId())
             .execute();
-        }
-        EipTTimeline timeline = Database.create(EipTTimeline.class);
-        timeline.setParentId(parentId);
-        timeline.setOwnerId(request.getUserId());
-        timeline.setAppId(request.getAppId());
-        timeline.setExternalId(request.getExternalId());
-        timeline.setNote(request.getTitle());
-        timeline.setTimelineType(EipTTimeline.TIMELINE_TYPE_ACTIVITY);
-        timeline.setParams(request.getPortletParams());
-        timeline.setPinned("F");
-        // 作成日
-        timeline.setCreateDate(tCal.getTime());
-        // 更新日
-        timeline.setUpdateDate(tCal.getTime());
+        } else {
+          EipTTimeline timeline = Database.create(EipTTimeline.class);
+          timeline.setParentId(parentId);
+          timeline.setOwnerId(request.getUserId());
+          timeline.setAppId(request.getAppId());
+          timeline.setExternalId(request.getExternalId());
+          timeline.setNote(request.getTitle());
+          timeline.setTimelineType(EipTTimeline.TIMELINE_TYPE_ACTIVITY);
+          timeline.setParams(request.getPortletParams());
+          timeline.setPinned("F");
+          // 作成日
+          timeline.setCreateDate(tCal.getTime());
+          // 更新日
+          timeline.setUpdateDate(tCal.getTime());
 
-        // タイムラインマップ追加
-        if (recipients != null && recipients.size() > 0) {
-          for (String recipient : recipients) {
+          // タイムラインマップ追加
+          if (recipients != null && recipients.size() > 0) {
+            for (String recipient : recipients) {
+              EipTTimelineMap timelineMap =
+                Database.create(EipTTimelineMap.class);
+              timelineMap.setLoginName(recipient);
+              timelineMap.setEipTTimeline(timeline);
+              timelineMap.setIsRead(priority == 1f ? 0 : 1);
+            }
+          } else {
             EipTTimelineMap timelineMap =
               Database.create(EipTTimelineMap.class);
-            timelineMap.setLoginName(recipient);
+            timelineMap.setLoginName("-1");
             timelineMap.setEipTTimeline(timeline);
-            timelineMap.setIsRead(priority == 1f ? 0 : 1);
+            timelineMap.setIsRead(1);
           }
-        } else {
-          EipTTimelineMap timelineMap = Database.create(EipTTimelineMap.class);
-          timelineMap.setLoginName("-1");
-          timelineMap.setEipTTimeline(timeline);
-          timelineMap.setIsRead(1);
         }
 
         Database.commit();
